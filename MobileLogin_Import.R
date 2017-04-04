1d02e59f9f8b88d3ed3f9c36a855076d3a15a2fd
Sigmium <donald.gennetten@me.com>
2017-04-04 11:25:58 -0400

    Added iOS device extraction

--- MobileLogin_Import.R
+++ MobileLogin_Import.R
@@ -1,6 +1,10 @@
 # load required libraries
 library(readr)
-library(data.table) 
+library(data.table)
+library(httr)
+library(curl)
+library(jsonlite)
+library(rvest)
 
 # Set location of source files as working directory
 # read_csv(choose.files()) to open dialogue box to select file. Same for write_csv
@@ -12,13 +16,22 @@ filelist <- list.files(pattern="*.csv")
 # Import and merge availabe source data into temporary dataframe 
 Login <- do.call(rbind, lapply(filelist, fread))
 
-# Import Google Play Supported Devices CSV
-  #URL Import:
+# Import Android Devices from Google Play Supported Devices ULR/CSV
+  #CSV Import via URL:
+    # TASK: Troubleshoot why this isnt working
     # GoogleSupportedDevices <- read_delim(url("http://storage.googleapis.com/play_public/supported_devices.csv"))
     # GoogleSupportedDevices <- read_delim(url("http://storage.googleapis.com/play_public/supported_devices.csv"), delim = ",")
- 
-GoogleSupportedDevices <- read_csv("c:/Users/TDP177/OneDrive - Capital One Financial Corporation/Data Science - Springboard/GooglePlaySupportedDevices.csv")
-  # Local File Import: (ISSUE: Seperated columns in Excel. Rows 3784 - 3787, 5212 & 13253 have errors due to comma in first variable)
+  #CSV Import after download & column split: 
+    # TASKS: Rows 3784 - 3787, 5212 & 13253 have errors due to comma in first value/column. Need to wrangle.    
+    GoogleSupportedDevices <- read_csv("c:/Users/TDP177/OneDrive - Capital One Financial Corporation/Data Science - Springboard/GooglePlaySupportedDevices.csv")
+
+# Import iOS Devices from theiphonewiki.com
+  # Wiki API Help URL https://www.wikidata.org/w/api.php
+  # The iPhone Wiki URL https://www.theiphonewiki.com/wiki/Models
+  # TASKS: Need to study and build HTTP API extract process  
+
+url <- "https://www.theiphonewiki.com/wiki/Models"
+GET(url)
 
 # Write data to output file for later analysis
 write_csv(Login, "c:/myfiles/original_MobileLogin.csv")
