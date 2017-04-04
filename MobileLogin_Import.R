5000f06319bc633f03bd11f13fb57218630e6152
Sigmium <donald.gennetten@me.com>
2017-04-04 10:11:38 -0400

    Add Google Play supported devices import
    Added lines to import Google Play supported devices

--- MobileLogin_Import.R
+++ MobileLogin_Import.R
@@ -12,5 +12,14 @@ filelist <- list.files(pattern="*.csv")
 # Import and merge availabe source data into temporary dataframe 
 Login <- do.call(rbind, lapply(filelist, fread))
 
+# Import Google Play Supported Devices CSV
+  #URL Import:
+    # GoogleSupportedDevices <- read_delim(url("http://storage.googleapis.com/play_public/supported_devices.csv"))
+    # GoogleSupportedDevices <- read_delim(url("http://storage.googleapis.com/play_public/supported_devices.csv"), delim = ",")
+ 
+GoogleSupportedDevices <- read_csv("c:/Users/TDP177/OneDrive - Capital One Financial Corporation/Data Science - Springboard/GooglePlaySupportedDevices.csv")
+  # Local File Import: (ISSUE: Seperated columns in Excel. Rows 3784 - 3787, 5212 & 13253 have errors due to comma in first variable)
+
 # Write data to output file for later analysis
-write_csv(original_MobileLogin.csv, "c:/myfiles")
+write_csv(Login, "c:/myfiles/original_MobileLogin.csv")
+write_csv(GoogleSupportedDevices, "c:/myfiles/original_GoogleSupportedDevices.csv")
