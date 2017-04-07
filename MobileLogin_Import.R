1328c2b162dcfb8444b90e508dc87fe28a1630f0
Sigmium <donald.gennetten@me.com>
2017-04-06 20:40:57 -0400

    Update MobileLogin_Import.R
    General cleanup

--- MobileLogin_Import.R
+++ MobileLogin_Import.R
@@ -5,34 +5,17 @@ library(httr)
 library(curl)
 library(jsonlite)
 library(rvest)
+library(xml2)
 
 # Set location of source files as working directory
-# read_csv(choose.files()) to open dialogue box to select file. Same for write_csv
-setwd("c:/myfiles")
+setwd("c:/myFiles")
 
 # Get list of available CSV files
-filelist <- list.files(pattern="*.csv")
+# TODO: 'login_' prefix is to ensure only login data files are pulled into filelist, but is broken and needs fixing 
+filelist <- list.files(pattern="login_*.csv")
 
-# Import and merge availabe source data into temporary dataframe 
+# Import and merge availabe source login data into temporary dataframe 
 Login <- do.call(rbind, lapply(filelist, fread))
 
-# Import Android Devices from Google Play Supported Devices ULR/CSV
-  #CSV Import via URL:
-    # TASK: Troubleshoot why this isnt working
-    # GoogleSupportedDevices <- read_delim(url("http://storage.googleapis.com/play_public/supported_devices.csv"))
-    # GoogleSupportedDevices <- read_delim(url("http://storage.googleapis.com/play_public/supported_devices.csv"), delim = ",")
-  #CSV Import after download & column split: 
-    # TASKS: Rows 3784 - 3787, 5212 & 13253 have errors due to comma in first value/column. Need to wrangle.    
-    GoogleSupportedDevices <- read_csv("c:/Users/TDP177/OneDrive - Capital One Financial Corporation/Data Science - Springboard/GooglePlaySupportedDevices.csv")
-
-# Import iOS Devices from theiphonewiki.com
-  # Wiki API Help URL https://www.wikidata.org/w/api.php
-  # The iPhone Wiki URL https://www.theiphonewiki.com/wiki/Models
-  # TASKS: Need to study and build HTTP API extract process  
-
-url <- "https://www.theiphonewiki.com/wiki/Models"
-GET(url)
-
 # Write data to output file for later analysis
-write_csv(Login, "c:/myfiles/original_MobileLogin.csv")
-write_csv(GoogleSupportedDevices, "c:/myfiles/original_GoogleSupportedDevices.csv")
+write_csv(Login, "original_MobileLogin.csv")
