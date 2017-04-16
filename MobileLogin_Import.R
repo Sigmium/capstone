3c7cecff76683b0550f4939667b9a29065949074
Sigmium <donald.gennetten@me.com>
2017-04-16 16:30:19 -0400

    Update MobileLogin_Import.R
    General cleanup

--- MobileLogin_Import.R
+++ MobileLogin_Import.R
@@ -1,18 +1,12 @@
 # load required libraries
 library(readr)
 library(data.table)
-library(httr)
-library(curl)
-library(jsonlite)
-library(rvest)
-library(xml2)
 
 # Set location of source files as working directory
-setwd("c:/myFiles")
+setwd("c:/myFiles/raw_data")
 
 # Get list of available CSV files
-# TODO: 'login_' prefix is to ensure only login data files are pulled into filelist, but is broken and needs fixing 
-filelist <- list.files(pattern="login_*.csv")
+filelist <- list.files(pattern="*.csv")
 
 # Import and merge availabe source login data into temporary dataframe 
 Login <- do.call(rbind, lapply(filelist, fread))
